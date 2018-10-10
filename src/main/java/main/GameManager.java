11f292d6e832be046e92716ad634e150e129ff2d
Carlos Ty <carty772@gmail.com>
2018-10-10 14:09:32 -0500

    Implemented the Card UI to Game Manager

--- src/main/java/main/GameManager.java
+++ src/main/java/main/GameManager.java
@@ -1,12 +1,19 @@
 package main;
 
+import formattedItems.CardClass;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class GameManager
 {
@@ -28,7 +35,7 @@ public class GameManager
 
         Button button1 = new Button("Initial Screen");
         button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+
             public void handle(ActionEvent event) {
                 getGameManager().cardScene1();
             }
@@ -41,35 +48,99 @@ public class GameManager
     }
     public void cardScene1()
     {
+        BorderPane layout = new BorderPane();
+        VBox cardChoices= new VBox(20);
+        VBox team1Backlog= new VBox(20);
+        VBox team2Backlog= new VBox(20);
+        HBox confirmCards = new HBox(20);
+
+        //Buttons to confirm card selection and to move to next card select screen.
+        //HBox will hold this button(s).
         this.stage.setTitle("Dealing Cards Team 1");
 
-        Button button1 = new Button("Dealing Cards Team 1");
+        Button button1 = new Button("Confirm Cards for Team 1");
+        confirmCards.setAlignment(Pos.CENTER);
+        confirmCards.getChildren().add(button1);
         button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+
             public void handle(ActionEvent event) {
                 getGameManager().cardScene2();
+                // Add logic to show which cards are selected and add them to "played" database.
             }
         });
-        HBox hbox = new HBox(button1);
-        Scene scene = new Scene(hbox, 400, 400);
+
+
+
+        //Game logic to generate cards from database of cards and references card class for formatting
+        for (int i=0; i<4;i++){
+            //Will replace this with logic to pull cards from database
+
+            Text dummyTitle= new Text("Test Title");
+            Text dummyText= new Text("This is an example of card text");
+
+            CardClass newCard= new CardClass(dummyTitle,dummyText);
+            cardChoices.getChildren().add(newCard.getCard());
+        }
+
+
+
+        layout.setCenter(cardChoices);
+        //layout.setLeft(); For Team 1 Backlog
+        //layout.setRight(); For Team 2 Backlog
+        layout.setBottom(confirmCards);
+
+
+        Scene scene = new Scene(layout, 600, 600);
         this.stage.setScene(scene);
         this.stage.show();
         this.stage.setScene(scene);
         this.stage.show();
     }
+
     public void cardScene2()
     {
+
+        BorderPane layout = new BorderPane();
+        VBox cardChoices= new VBox(20);
+        VBox team1Backlog= new VBox(20);
+        VBox team2Backlog= new VBox(20);
+        HBox confirmCards = new HBox(20);
+
         this.stage.setTitle("Dealing Cards Team 2");
 
-        Button button1 = new Button("Dealing Cards Team 2");
+        Button button1 = new Button("Confirm Cards For Team 2");
+        confirmCards.setAlignment(Pos.CENTER);
+        confirmCards.getChildren().add(button1);
         button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+
             public void handle(ActionEvent event) {
                 getGameManager().revealCards();
+                // Add logic to show which cards are selected and add them to "played" database.
             }
         });
-        HBox hbox = new HBox(button1);
-        Scene scene = new Scene(hbox, 400, 400);
+
+
+
+        //Game logic to generate cards from database of cards and references card class for formatting
+        for (int i=0; i<4;i++){
+            //Will replace this with logic to pull cards from database
+
+            Text dummyTitle= new Text("Test Title");
+            Text dummyText= new Text("This is an example of card text");
+
+            CardClass newCard= new CardClass(dummyTitle,dummyText);
+            cardChoices.getChildren().add(newCard.getCard());
+        }
+
+
+
+        layout.setCenter(cardChoices);
+        //layout.setLeft(); For Team 1 Backlog
+        //layout.setRight(); For Team 2 Backlog
+        layout.setBottom(confirmCards);
+
+
+        Scene scene = new Scene(layout, 600, 600);
         this.stage.setScene(scene);
         this.stage.show();
         this.stage.setScene(scene);
@@ -81,7 +152,7 @@ public class GameManager
 
         Button button1 = new Button("Revealing cards");
         button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+
             public void handle(ActionEvent event) {
                 getGameManager().quizScene1();
             }
@@ -99,7 +170,7 @@ public class GameManager
 
         Button button1 = new Button("Quiz Team 1");
         button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+
             public void handle(ActionEvent event) {
                 getGameManager().quizScene2();
             }
@@ -117,7 +188,7 @@ public class GameManager
 
         Button button1 = new Button("Quiz Team 2");
         button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+
             public void handle(ActionEvent event) {
                 getGameManager().extraCard();
             }
@@ -135,7 +206,7 @@ public class GameManager
 
         Button button1 = new Button("Play Extra Card");
         button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+
             public void handle(ActionEvent event) {
                 getGameManager().spendPoints1();
             }
@@ -153,7 +224,7 @@ public class GameManager
 
         Button button1 = new Button("Spend Points 1");
         button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+
             public void handle(ActionEvent event) {
                 getGameManager().spendPoints2();
             }
@@ -171,7 +242,7 @@ public class GameManager
 
         Button button1 = new Button("Spend Points 2");
         button1.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
+
             public void handle(ActionEvent event) {
                 getGameManager().cardScene1();
             }
