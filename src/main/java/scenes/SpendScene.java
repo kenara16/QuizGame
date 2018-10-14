38c1fc77e196869d466a451f8d95ec3179e789cc
Jarret <jarret.a.miller@gmail.com>
2018-10-14 03:02:23 -0500

    added scenes

---
+++ src/main/java/scenes/SpendScene.java
@@ -0,0 +1,58 @@
+package scenes;
+
+import backend.TeamClass;
+import formattedItems.BacklogDisplay;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import main.GameManager;
+
+public class SpendScene implements IGameScene {
+    public String title = "Spend Scene";
+    public String getTitle()
+    {
+        return title;
+    }
+    public IGameScene getNext()
+    {
+        {
+            if (GameManager.currentlyFirstTeam())
+            {
+                return new SpendScene();
+            }
+            else
+            {
+                return new CardScene();
+            }
+        }
+    }
+    public Scene getScene()
+    {
+
+        Button button1 = new Button("Spend Points for Team " + GameManager.getTeamNumber());
+        button1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                SceneManager.nextScene();
+            }
+        });
+        BorderPane layout = new BorderPane();
+        layout.setBottom(button1);
+        VBox teamBacklog= new BacklogDisplay(GameManager.getCurrentTeam()).getHBox();
+        if(GameManager.currentlyFirstTeam())
+        {
+            layout.setLeft(teamBacklog);
+        }
+        else
+        {
+            layout.setRight(teamBacklog);
+        }
+        Scene scene = new Scene(layout, 400, 400);
+        return scene;
+    }
+
+}
