260ae220a7d5284c7daa6ff1244d6c3780688fcb
Youngjo Kim <wangjoe22@gmail.com>
2018-10-28 12:45:31 -0500

    Uncommented a code which rearranging data in DB.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -22,7 +22,7 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
-//        DBConnectionManager.PleaseDontTouchThisMethod();
+        DBConnectionManager.PleaseDontTouchThisMethod();
 
 
 // This code is for testing DB part.
