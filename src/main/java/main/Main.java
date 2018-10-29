8fa4f58fe0d6c14c2e99261eee413e5468b04109
Bilader Benjiamin <bilader.benjamin@gmail.com>
2018-10-28 20:54:43 -0500

    test

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -24,6 +24,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         DBConnectionManager.PleaseDontTouchThisMethod();
 
+        //test
 
 // This code is for testing DB part.
 //        List<Card> cards;
